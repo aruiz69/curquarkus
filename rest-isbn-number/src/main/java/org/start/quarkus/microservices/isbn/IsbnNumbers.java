cb14ae5fa0dc8f30db60e6f04e28a5bffd2a26ec
Israel Ruiz <alvaro.ruiz@neoris.com>
2022-11-18 13:34:55 -0600

    step 2

--- rest-isbn-number/src/main/java/org/start/quarkus/microservices/isbn/IsbnNumbers.java
+++ rest-isbn-number/src/main/java/org/start/quarkus/microservices/isbn/IsbnNumbers.java
@@ -1,9 +1,23 @@
 package org.start.quarkus.microservices.isbn;
 
+import javax.json.bind.annotation.JsonbProperty;
+import javax.json.bind.annotation.JsonbTransient;
 import java.time.Instant;
 
 public class IsbnNumbers {
+    @JsonbProperty("isbn_13")
     public String isbn13;
+    @JsonbProperty("isbn_10")
     public String isbn10;
+    @JsonbTransient
     public Instant generationDate;
+
+    @Override
+    public String toString() {
+        return "IsbnNumbers{" +
+                "isbn13='" + isbn13 + '\'' +
+                ", isbn10='" + isbn10 + '\'' +
+                ", generationDate=" + generationDate +
+                '}';
+    }
 }
