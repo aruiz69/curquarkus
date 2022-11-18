6acba01b6cf77ce620a8916eb74c82107fe356b6
Israel Ruiz <alvaro.ruiz@neoris.com>
2022-11-18 15:39:58 -0600

    step book changes

--- rest-book/src/main/java/org/start/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/start/quarkus/microservices/book/BookResource.java
@@ -1,17 +1,37 @@
 package org.start.quarkus.microservices.book;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.jboss.logging.Logger;
+import org.jboss.logging.annotations.Param;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.time.Instant;
 
 @Path("/api/books")
 public class BookResource {
+    @Inject
+    Logger logger;
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createABook(
+            @FormParam("title") String title,
+            @FormParam("author") String author,
+            @FormParam("yearOfPublication") int yearOfPublication,
+            @FormParam("genre") String genre ){
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+        Book book = new Book();
+        book.isbn13 = "We will get it from the Number Microservice";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublicaton = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+        logger.info("Book created :" + book);
+        return Response.status(201).entity(book).build();
+
+
     }
 }
