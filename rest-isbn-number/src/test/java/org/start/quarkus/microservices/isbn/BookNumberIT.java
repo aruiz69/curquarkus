8bd75cd2c43fd6daa4a850e6ed4c5aeecbff8c65
Israel Ruiz <alvaro.ruiz@neoris.com>
2022-11-18 15:30:10 -0600

    step 5 test unit

--- rest-isbn-number/src/test/java/org/start/quarkus/microservices/isbn/BookNumberIT.java
+++
@@ -1,8 +0,0 @@
-package org.start.quarkus.microservices.isbn;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class BookNumberIT extends BookNumberTest {
-    // Execute the same tests but in packaged mode.
-}
--- rest-isbn-number/src/test/java/org/start/quarkus/microservices/isbn/BookNumberTest.java
+++ rest-isbn-number/src/test/java/org/start/quarkus/microservices/isbn/BookNumberTest.java
@@ -4,18 +4,20 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 
 @QuarkusTest
 public class BookNumberTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void shouldGenerateIsNumbersISBN() {
         given()
-          .when().get("/api/isbn")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .when().get("/api/isbn")
+                .then()
+                .statusCode(200)
+                .body("isbn_13", startsWith("13-"))
+                .body("isbn_10", startsWith("10-"))
+                .body(not(hasItem("generationDate")));
     }
 
 }
