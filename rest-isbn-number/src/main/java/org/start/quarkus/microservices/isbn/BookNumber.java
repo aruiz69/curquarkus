10529dfda194ea964438917f2899f274ef4666af
Israel Ruiz <alvaro.ruiz@neoris.com>
2022-11-18 13:07:53 -0600

    step 1

--- rest-isbn-number/src/main/java/org/start/quarkus/microservices/isbn/BookNumber.java
+++ rest-isbn-number/src/main/java/org/start/quarkus/microservices/isbn/BookNumber.java
@@ -1,16 +1,28 @@
 package org.start.quarkus.microservices.isbn;
 
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.time.Instant;
+import java.util.Random;
 
 @Path("/api/isbn")
 public class BookNumber {
+    @Inject
+    Logger logger;
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces(MediaType.APPLICATION_JSON)
+    public IsbnNumbers generateIsbNumbers() {
+        IsbnNumbers isbnNumbers = new IsbnNumbers();
+        isbnNumbers.isbn13 = "13-" + new Random().nextInt(100_000_000);
+        isbnNumbers.isbn10 = "10-" + new Random().nextInt(100_000);
+        isbnNumbers.generationDate = Instant.now();
+        logger.info(String.format("Numbers generated %s", isbnNumbers));
+        return isbnNumbers;
     }
 }
---
+++ rest-isbn-number/src/main/java/org/start/quarkus/microservices/isbn/IsbnNumbers.java
@@ -0,0 +1,9 @@
+package org.start.quarkus.microservices.isbn;
+
+import java.time.Instant;
+
+public class IsbnNumbers {
+    public String isbn13;
+    public String isbn10;
+    public Instant generationDate;
+}
